a0d4ad6c49f0c2bcde56ce1ecafab598de2f5539
Shikha <shikhagpt29@gmail.com>
2022-04-04 07:18:48 +0530

    Update initial reelSet

--- ReelView.js
+++ ReelView.js
@@ -1,6 +1,10 @@
 var ReelView = /** @class */ (function () {
     var reels = [];
-    var relPos = ["hv2", "lv3", "lv3"];
+    var relPos = [["hv2", "lv3", "lv3"],
+                 ["hv1", "lv1", "lv2"],
+                 ["hv2", "lv3", "lv1"],
+                 ["hv3", "lv1", "lv2"],
+                 ["hv2", "lv2", "lv1"],];
     var symW = 128;
 
     function mainF() {
@@ -16,7 +20,7 @@ var ReelView = /** @class */ (function () {
         reelView.name = "reel_" + reelId;
         reelView.position.x = reelId * symW + offSet * reelId;
         for (var symId = 0; symId < 3; symId++) {
-            var sprite = Symbol(relPos[symId]);
+            var sprite = Symbol(relPos[reelId][symId]);
             sprite.position.y = symId * symW + offSet * symId;
             reelView.addChild(sprite);
         }
